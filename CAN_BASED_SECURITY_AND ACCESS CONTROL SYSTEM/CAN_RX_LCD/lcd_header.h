ce65dee8777bc67c6b337e49b9b4ddd53f27a949
sakthikarthi <128763168+Sakthikarthi86@users.noreply.github.com>
2025-12-14 14:14:40 +0530

    Add files via upload

---
+++ CAN_BASED_SECURITY_AND ACCESS CONTROL SYSTEM/CAN_RX_LCD/lcd_header.h
@@ -0,0 +1,108 @@
+#define lcd_d1 0xf<<10
+
+#define rs 1<<8
+
+#define E 1<<9
+
+void lcd_init(void);
+
+void lcd_cmd(unsigned char);
+
+void lcd_data(unsigned char);
+
+void delay(int);
+
+
+void lcd_init(void)
+
+{
+
+	IODIR0 |=lcd_d1|rs|E;
+
+	lcd_cmd(0x01);
+
+	lcd_cmd(0x02);
+
+	lcd_cmd(0x0c);
+
+	lcd_cmd(0x28);
+
+	lcd_cmd(0x80);
+
+}
+
+void lcd_cmd(unsigned char cmd)
+
+{
+
+	IOCLR0=lcd_d1;
+
+	IOSET0=(0xf0&cmd)<<6;
+
+	IOCLR0=rs;
+
+	IOSET0=E;
+
+	delay(2);
+
+	IOCLR0=E;
+
+
+	IOCLR0=lcd_d1;
+
+	IOSET0=(0xf&cmd)<<10;
+
+	IOCLR0=rs;
+
+	IOSET0=E;
+
+	delay(2);
+
+	IOCLR0=E;
+
+}
+
+void lcd_data(unsigned char d)
+
+{
+
+	IOCLR0=lcd_d1;
+
+	IOSET0=(0xf0&d)<<6;
+
+	IOSET0=rs;
+
+	IOSET0=E;
+
+	delay(2);
+
+	IOCLR0=E;
+
+
+	IOCLR0=lcd_d1;
+
+	IOSET0=(0xf&d)<<10;
+
+	IOSET0=rs;
+
+	IOSET0=E;
+
+	delay(2);
+
+	IOCLR0=E;
+
+}
+
+void lcd_string(unsigned char *s)
+
+{
+
+	while(*s)
+
+	{
+
+		lcd_data(*s++);
+
+	}
+
+}
---
+++ CAN_BASED_SECURITY_AND ACCESS CONTROL SYSTEM/CAN_RX_LCD/main_rx.c
@@ -0,0 +1,95 @@
+/* main_nodeB_RX.c */
+
+#include "header.h"
+#include"uart_driver.c"
+#include"can2_driver.c"
+void delay(int ms)
+{
+
+	T0PR=60000-1;
+
+	T0TCR=0x01;
+
+	while(T0TC<ms);
+
+	T0TCR=0x03;
+
+	T0TCR=0x00;
+
+}
+#include"lcd_header.h"
+
+main(){	
+
+	CAN2_MSG m1;
+
+	lcd_init();
+
+	can2_init();
+
+	uart0_init(115200);
+
+	lcd_cmd(0x80);
+
+	lcd_string("CAN BASED SECURITY AND ACCESS CONTROL SYSTEM");
+
+	while(1){		
+
+		can2_rx(&m1);
+
+		
+
+		if(m1.rtr==0){
+					uart0_tx_hex(m1.id);
+			uart0_tx(' ');
+			uart0_tx_hex(m1.dlc);
+			uart0_tx(' ');
+			uart0_tx_hex(m1.byteA);
+			uart0_tx(' ');
+			uart0_tx_hex(m1.byteB);
+			uart0_tx_string("\r\n");
+
+			//uart0_tx_string("nodeB: DATA-FRAME is received\r\n");
+
+			if((m1.byteA)==1)
+			{
+				lcd_cmd(0x01);
+				lcd_cmd(0x80);
+				lcd_string("Access granted");
+				uart0_tx_string("access granted\r\n");
+				//delay(1000);
+			}
+			else if((m1.byteA)==0)
+			{
+				lcd_cmd(0x01);
+				lcd_cmd(0x80);
+				lcd_string("Access denied");
+				uart0_tx_string("access denied\r\n");
+				//delay(1000);
+			}
+			delay(500);
+
+		}	
+
+		else{
+
+			uart0_tx_string("nodeB: REMOTE-FRAME is received\r\n");
+
+			uart0_tx_hex(m1.id);
+
+			uart0_tx(' ');
+
+			uart0_tx_hex(m1.dlc);
+
+			uart0_tx(' ');
+
+			uart0_tx_string("\r\n");
+
+		}	
+
+	}
+
+	
+
+}
+
